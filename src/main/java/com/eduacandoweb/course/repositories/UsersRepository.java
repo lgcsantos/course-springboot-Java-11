07169b85d48db007338adde8b3ca3c5b09843184
lgcsantos <lucas.prog@outlook.com>
2022-06-27 22:09:57 -0300

    Product

--- src/main/java/com/eduacandoweb/course/repositories/UsersRepository.java
+++ src/main/java/com/eduacandoweb/course/repositories/UsersRepository.java
@@ -1,6 +1,7 @@
 package com.eduacandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.eduacandoweb.course.entities.Users;
 
 
---
+++ src/main/java/com/eduacandoweb/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.eduacandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduacandoweb.course.entities.Category;
+import com.eduacandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long Id) {
+		Optional<Category> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+}
